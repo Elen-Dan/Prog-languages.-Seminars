ef087a722a3e8939aef3de0b1fa7c7e099162881
El_Dan <lilit-li@ya.ru>
2022-08-19 18:22:10 +0400

    Задача 8. Вывод четных чисел

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показывает чётные числа от 1 до N
+
+Console.Write("Введите число: ");
+string? a = Console.ReadLine();
+int count = 1;
+while (count < Int32.Parse(a))
+    {
+        if (count % 2 == 0)
+            {
+                Console.WriteLine("Четные числа от 1 до "+ a +": " + count);
+            }
+        
+        count = count + 1; 
+    }
+// Конец
