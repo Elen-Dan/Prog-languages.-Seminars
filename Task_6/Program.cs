a84e3d44e0bd8ff78f3b98e17296e9c21dd0cf6d
El_Dan <lilit-li@ya.ru>
2022-08-19 16:38:01 +0400

    Задача 6. Чёт - нечёт

---
+++ Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Чёт-нечёт
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+string? a = Console.ReadLine();
+
+if (Int32.Parse(a) % 2 == 0)
+    {
+        Console.WriteLine("Число является четным");
+    }
+else 
+    {
+        Console.WriteLine("Число является нечетным");
+    }    
