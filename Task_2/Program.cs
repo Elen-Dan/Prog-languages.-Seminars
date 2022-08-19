e43f9d91f116a7b74fadd96c6f483d19558c52f9
El_Dan <lilit-li@ya.ru>
2022-08-19 13:32:52 +0400

    Задача 2. Большее из двух чисел

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+string? a = Console.ReadLine();
+Console.WriteLine("Введите второе число ");
+string? b = Console.ReadLine();
+if (Int32.Parse(a) > Int32.Parse(b))
+    {
+        Console.WriteLine("Число " + a + " больше, чем " + b);
+    }
+else 
+    {
+        Console.WriteLine ("Число " + b + " больше, чем " + a);
+    }
